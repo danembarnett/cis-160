56bdaef21264e3d2089cab7240c914836f4b71c1
Dane Barnett <danebarnettmail@yahoo.com>
2018-08-22 13:29:28 -0400

    Add files via upload

--- CIS-160/project5/class/Student.cpp
+++ CIS-160/project5/class/Student.cpp
@@ -1,17 +1,81 @@
-/*      Name: Dane Barnett
-        Class: CIS 160 ME1
-        Date: March 29, 2017
-        Program: St Joseph Hospital Billing
-        Description: A program that computes and print a statement of charges
-                     for patience being discharged from the hospital.
-*/
+//Student.cpp
+
+
 #include <iostream>
 
 #include "Student.h"
 
-
 using namespace std;
 
-void Student::setID() {
-    cout << "Dane" << endl;
+// default constructor initialize member variable at the start of the object call.
+Student::Student()
+{
+    name = "";
+    id = 0;
+    score1 = 0;
+    score2 = 0;
+    score3 = 0;
+    avg = 0;
+    grade = ' ';
+}
+// destructor is used to destruct the object at the end of the program .
+Student::~Student() {}
+
+// sets value for all private member variables.
+void Student::setInput()
+{
+    cout << "Enter Name : "; getline(cin, name);
+    cout << "Enter ID : "; cin >> id;
+    cout << "Enter Test 1 Score : "; cin >> score1;
+    cout << "Enter Test 2 Score : "; cin >> score2;
+    cout << "Enter Test 3 Score : "; cin >> score3;
+    avg = getAVG();
+    grade = getGrade();
+}
+
+// return float for the average of member variable score1, score2 and score3.
+float Student::getAVG() const
+
+{
+    return (score1 + score2 + score3) / 3;
+}
+
+// converts float avg and return char grade.
+char Student::getGrade() const
+{
+    if (avg >= 90)
+        return 'A';
+    else if (avg >= 80 && avg < 90)
+        return 'B';
+    else if (avg >= 70 && avg < 80)
+        return 'C';
+
+    else if (avg >= 60 && avg < 70)
+        return 'D';
+
+    else {
+        return 'F';
+    }
+}
+
+// return string to determine if the student pass or fail.
+string Student::passFail() const
+{
+    if (avg >= 70)
+        return "You Passed.\n";
+    else {
+        return "You Failed.\n";
+    }
+}
+
+// prints out all member variable and member function.
+void Student::getOutput() const
+{
+    cout << "\nEmployee Name :\t" << name << endl;
+    cout << "ID Number :\t\t" << id << endl;
+    cout << "Test :\n\t1-    " << score1 << endl;
+    cout << "\t2-    " << score2 << endl;
+    cout << "\t3-    " << score3 << endl;
+    printf("Average :\t%2.2f\n", getAVG());
+    cout << "Grade :\t\t" << getGrade() << "    "<< passFail();
 }
--- CIS-160/project5/class/Student.h
+++ CIS-160/project5/class/Student.h
@@ -1,15 +1,33 @@
-#include <iostream>
+//Student.h
 
+//Header file used to declare member variable and member function.
+#ifndef PROJECTC_STUDENT_H  // Guards
+#define PROJECTC_STUDENT_H  // Guards
 
-#ifndef STUDENT_H
-#define STUDENT_H
 using namespace std;
+class Student
+{
+// private cannot be accessed from outside programs
+private:
+    // member variables.
+    string name;
+    int id;
+    short score1, score2, score3;
+    float avg;
+    char grade;
 
-class Student {
+    // member function
+    float getAVG() const;         // return float for the average of member variable score1, score2 and score3.
+    char getGrade() const;        // converts float avg and return char grade.
+    string passFail() const;      // return string to determine if the student pass or fail.
 
+//public can be access by other programs such as main.
 public:
-	void setID();
+    Student();              // default constructor.
+    ~Student();             // destructor.
+    void setInput();   // sets value for all private member variables.
+    void getOutput() const; // prints out all member variable and member function.
 
 };
 
-#endif
+#endif                      // Guards
